972dde6801570257dab303dd3e06855df391e598
Andy Pliszka <andy@isp44.com>
2012-10-21 01:49:23 -0400

    New Tasks app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = 'e965a9b7e40baa668fa75f847fde0ac1230b257b2a4b30e59ea692a35c77103016e5969c7dfa6664a13bb1c41d7f3993509253c6a993472fb1c8c97494b72b9f'
